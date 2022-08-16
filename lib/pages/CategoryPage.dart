30216117ee485c1f67bad0dce41185d283f88f94
Msalati <ItsMsalati@gmail.com>
2022-08-16 10:42:19 +0200

    Categories page with firebase ✅ Added courses list [under development]

--- lib/pages/CategoryPage.dart
+++ lib/pages/CategoryPage.dart
@@ -1,81 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:graduation_project/Widgets/Category_Widget.dart';
+import 'package:graduation_project/providers/categoryStateProvider.dart';
 
-class CategoryPage extends StatelessWidget {
-  const CategoryPage({Key? key}) : super(key: key);
+class CategoryPage extends StatefulWidget {
+  const CategoryPage({super.key});
 
+  @override
+  State<CategoryPage> createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+  var _toggleFavorite;
+
+  List<String> docIDs = [];
+
+  CategoryState categoryRef = new CategoryState();
+
+  Future getDocIds() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .get()
+        .then(((snapshot) {
+      snapshot.docs.forEach((element) {
+        categoryRef.setCategories(element.data());
+        // <UserState>().setUser(auth.currentUser);
+        // print(element.data()['firstname']);
+      });
+    }));
+  }
+
+  void initState() {
+    super.initState();
+    getDocIds();
+  }
+
+  // ···
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: SingleChildScrollView(
-          child: Expanded(
-            child: Column(
-              children: [
-                CategoryWidget(
-                  icon: Icons.account_balance_outlined,
-                  text: 'تعليم اساسي',
-                  startColor: 'ADD8E6',
-                  endColor: '00008B',
-                  sizedBoxHeight: 20,
-                ),
-                CategoryWidget(
-                  icon: Icons.account_balance_outlined,
-                  text: 'تعليم اساسي',
-                  startColor: 'ffafbd ',
-                  endColor: 'ffc3a0',
-                  sizedBoxHeight: 20,
-                ),
-                CategoryWidget(
-                  icon: Icons.account_balance_outlined,
-                  text: 'تعليم اساسي',
-                  startColor: '2193b0 ',
-                  endColor: '6dd5ed',
-                  sizedBoxHeight: 20,
-                ),
-                CategoryWidget(
-                  icon: Icons.account_balance_outlined,
-                  text: 'تعليم اساسي',
-                  startColor: 'cc2b5e ',
-                  endColor: '753a88',
-                  sizedBoxHeight: 20,
-                ),
-                CategoryWidget(
-                  icon: Icons.account_balance_outlined,
-                  text: 'تعليم اساسي',
-                  startColor: '06beb6 ',
-                  endColor: '48b1bf',
-                  sizedBoxHeight: 20,
-                ),
-                CategoryWidget(
-                  icon: Icons.account_balance_outlined,
-                  text: 'تعليم اساسي',
-                  startColor: 'ffafbd ',
-                  endColor: 'ffc3a0',
-                  sizedBoxHeight: 20,
-                ),
-                CategoryWidget(
-                  icon: Icons.account_balance_outlined,
-                  text: 'تعليم اساسي',
-                  startColor: '2193b0 ',
-                  endColor: '6dd5ed',
-                  sizedBoxHeight: 20,
-                ),
-                CategoryWidget(
-                  icon: Icons.account_balance_outlined,
-                  text: 'تعليم اساسي',
-                  startColor: 'cc2b5e ',
-                  endColor: '753a88',
-                  sizedBoxHeight: 20,
-                ),
-
-
-              ],
-            ),
-          ),
-        ),
-      ),
+      margin: const EdgeInsets.only(top: 10),
+      child: StreamBuilder(
+          stream:
+              FirebaseFirestore.instance.collection('categories').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((document) {
+                return Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    height: MediaQuery.of(context).size.width / 4,
+                    child: CategoryWidget(
+                      icon: Icons.menu_book,
+                      text: document['title'],
+                      startColor: 'f48A9C5',
+                      endColor: '48C9D7',
+                      sizedBoxHeight: 10,
+                    ),
+                  ),
+                );
+              }).toList(),
+            );
+          }),
     );
   }
 }
